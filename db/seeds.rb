4abb803b3796125b375fcb6e5b832b9c4ed4cb2b
Raul Vega <raulvprivate@gmail.com>
2014-11-04 11:52:06 +0100

    funciona nombre y ciudad

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 eje1 = Location.create(name: 'Casta', city: 'Barcelona', zip_code: 28900, country: 'Spain', street: 'Calle Castellana', description: 'Esto es una descripcion bastante explicativa.')
-eje2 = Location.create(name: 'Humildat', city: 'Malaga', zip_code: 66900, country: 'Spain', street: 'Santa Clara', description: 'Esto es una descripcion bastante explicativa.')
+eje2 = Location.create(city: 'Malaga', zip_code: 66900, country: 'Spain', street: 'Santa Clara', description: 'Esto es una descripcion bastante explicativa.')
 eje3 = Location.create(name: 'Valors', city: 'Madrid', zip_code: 43300, country: 'Spain', street: 'Canaletas', description: 'Esto es una descripcion bastante explicativa.')
